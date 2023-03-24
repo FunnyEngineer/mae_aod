fab6f68c49d1255f64b20670772f32ae5ee790dd
FunnyEngineer <funnyengineer@utexas.edu>
2023-03-23 22:53:16 -0500

    fix type

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -37,7 +37,6 @@ def train_one_epoch(model: torch.nn.Module,
         print('log_dir: {}'.format(log_writer.log_dir))
 
     for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-        pdb.set_trace()
 
         # we use a per iteration (instead of per epoch) lr scheduler
         if data_iter_step % accum_iter == 0:
